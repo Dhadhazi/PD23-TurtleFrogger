c6c933189d9881e6b706fe8cd74faa21a97dbc10
Dhadhazi <dhadhazi@gmail.com>
2020-11-24 13:43:50 +0100

    Moving turtle and car

--- main.py
+++ main.py
@@ -8,7 +8,17 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+car = CarManager()
+
+screen.listen()
+screen.onkey(player.move, "Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car.move()
+    if player.ycor() > player.finish_line:
+        player.reset()
+
